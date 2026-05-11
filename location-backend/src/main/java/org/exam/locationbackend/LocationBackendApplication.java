bf250caa806f7edb60e55d3bb5e6f1f224493a2a
freecoss <abderrahimfarahi99@gmail.com>
2026-05-11 13:27:56 +0100

    Initialisation Spring Boot

---
+++ location-backend/src/main/java/org/exam/locationbackend/LocationBackendApplication.java
@@ -0,0 +1,13 @@
+package org.exam.locationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationBackendApplication.class, args);
+    }
+
+}
---
+++ location-backend/src/test/java/org/exam/locationbackend/LocationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exam.locationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
